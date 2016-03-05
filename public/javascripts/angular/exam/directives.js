b48ac3fa33a3ed0dec90926939f8ba4073eeb0c1
RambleRainbow <three_rainbow@msn.com>
2016-03-05 22:03:03 +0800

    考试界面基本交互完成

--- public/javascripts/angular/exam/directives.js
+++ public/javascripts/angular/exam/directives.js
@@ -28,14 +28,52 @@ angular.module('examApp.directives')
         restrict: 'A',
         replace:true,
         scope:{
-            input: '='
+            input: '=',
+            answer: '='
+        },
+        link: function($scope, $ele, $attrs) {
+            $($ele).find('.numPad').each(function(idx, ele) {
+                $(this).on('touchstart', function() {
+                    $scope.$apply(function() {
+                        $scope.answer += $(ele).text();
+                    })
+                })
+            });
+
+            $($ele).find('.clearBtn').each(function(idx, ele) {
+                $(this).on('touchstart', function() {
+                    $scope.$apply(function() {
+                        $scope.answer = '';
+                    });
+                });
+            });
+
         },
         template:
-            '<div>' +
-            '<div class="row"><div class="span1 offset5 numPad">1</div><div class="span1 numPad">2</div><div class="span1 numPad">3</div></div>' +
-            '<div class="row"><div class="span1 offset5 numPad">4</div><div class="span1 numPad">5</div><div class="span1 numPad">6</div></div>' +
-            '<div class="row"><div class="span1 offset5 numPad">7</div><div class="span1 numPad">8</div><div class="span1 numPad">9</div></div>' +
-            '<div class="row"><div class="span3 offset5 numPad">0</div></div>' +
+            '<div class="row">' +
+                '<div class="span10">' +
+                    '<div class="row">' +
+                        '<button class="btn btn-primary span2 offset5 numPad">1</button>' +
+                        '<button class="btn btn-primary span2 numPad">2</button>' +
+                        '<button class="btn btn-primary span2 numPad">3</button>' +
+                    '</div>' +
+                    '<div class="row">' +
+                        '<button class="btn btn-primary span2 offset5 numPad">4</button>' +
+                        '<button class="btn btn-primary span2 numPad">5</button>' +
+                        '<button class="btn btn-primary span2 numPad">6</button>' +
+                    '</div>' +
+                    '<div class="row">' +
+                        '<button class="btn btn-primary span2 offset5 numPad">7</button>' +
+                        '<button class="btn btn-primary span2 numPad">8</button>' +
+                        '<button class="btn btn-primary span2 numPad">9</button>' +
+                    '</div>' +
+                    '<div class="row">' +
+                        '<button class="btn btn-primary span6 offset5 numPad">0</button>' +
+                    '</div>' +
+                '</div>' +
+                '<div class="span2">' +
+                    '<button class="btn span12 clearBtn" >清除</button>' +
+                '</div>' +
             '</div>'
     }
 })
--- public/javascripts/angular/exam/examController.js
+++ public/javascripts/angular/exam/examController.js
@@ -3,18 +3,18 @@
  */
 console.log('load controller');
 angular.module('examApp.controllers')
-    .controller( 'examController', ['$scope', function examController($scope) {
+.controller( 'examController', ['$scope', function examController($scope) {
+    $scope.curSubject = {
+        question: "11-3=?",
+        answer:"8",
+        input:"test"
+    }
+
+    $scope.nextSubject = function() {
         $scope.curSubject = {
-            question: "11-3=?",
-            answer:"8",
+            question:"12-9=?",
+            answer:"3",
             input:"test"
         }
-
-        $scope.nextSubject = function() {
-            $scope.curSubject = {
-                question:"12-9=?",
-                answer:"3",
-                input:"test"
-            }
-        }
-    }]);
+    }
+}]);
