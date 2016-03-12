f73e6ccb5266f9d527f7aa17add6fe5c44e45673
RambleRainbow <three_rainbow@msn.com>
2016-03-12 21:00:45 +0800

    修改20内加减的测试规则

--- tools/addExam.js
+++ tools/addExam.js
@@ -11,12 +11,12 @@ mongoClient.connect('mongodb://127.0.0.1:27017/gws', function(err, db) {
 
     var ex = [
         {
-            _id:1,
+            _id:2,
             name:"20以内加减法",
             questionQuery:JSON.stringify(
             {
                 $or: [
-                    {label: {$all: ["<=20"]}},
+                    {label: {$all: ["<=20","trade"]}},
                     {label: {$all: ["<=10"]}}
                 ]
             }
@@ -24,27 +24,27 @@ mongoClient.connect('mongodb://127.0.0.1:27017/gws', function(err, db) {
         }
     ];
 
-    //examRules.insert(ex,function(err, results) {
-    //    if(err) {
-    //        console.log(err);
-    //    }
-    //    else {
-    //        console.log('finised');
-    //    }
-    //    db.close();
-    //});
-    //
-    examRules.find({}).toArray().
-    then(function(results) {
-        r = results;
-        var q = JSON.parse(results[0].questionQuery);
-        return db.collection("questions").find(q).toArray();
-    }).
-    then(function(results) {
-        console.log(results.length);
-    })
-    .catch(function(err) {
-        console.log(err);
-    })
+    examRules.insert(ex,function(err, results) {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            console.log('finised');
+        }
+        db.close();
+    });
+
+    //examRules.find({}).toArray().
+    //then(function(results) {
+    //    r = results;
+    //    var q = JSON.parse(results[0].questionQuery);
+    //    return db.collection("questions").find(q).toArray();
+    //}).
+    //then(function(results) {
+    //    console.log(results.length);
+    //})
+    //.catch(function(err) {
+    //    console.log(err);
+    //})
 
 });
