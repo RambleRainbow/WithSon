53b3257ad6a3651b37c33cf7b78e3e43267c4d26
RambleRainbow <three_rainbow@msn.com>
2016-03-23 23:49:43 +0800

    对SUBJECT数据集加入TIMESPAN字段的批处理工具

---
+++ tools/updateSubjects.js
@@ -0,0 +1,32 @@
+/**
+ * Created by 灵 on 2016/3/23.
+ */
+var mongoClient = require('mongodb').MongoClient;
+var _ = require('underscore')._;
+var async = require('async');
+
+mongoClient.connect('mongodb://localhost:27017/gws', function(err, db) {
+    var examsCollection = db.collection('exams');
+    examsCollection.find({}).toArray()
+        .then(function(exams) {
+            _.each(exams, function(exam) {
+                _.each(exam.subjects, function(subject) {
+                    subject.timeSpan = new Date(subject.endTime) - new Date(subject.startTime)
+                });
+            });
+
+            var updatePromises = _.map(exams, function(exam) {
+                var id = exam._id;
+                delete exam._id;
+                return examsCollection.updateOne({_id:id},exam);
+            });
+
+            return Promise.all(updatePromises);
+        })
+        .then(function() {
+            return true;
+        })
+        .catch(function(err) {
+            console.log(err.message);
+        });
+});
